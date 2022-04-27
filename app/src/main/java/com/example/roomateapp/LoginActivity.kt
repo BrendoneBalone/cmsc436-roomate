e3a771d0c93e41504d81259333d54a97f9a009d6
Brendan Gillespy <brendan.gillespy@gmail.com>
2022-04-27 17:18:34 -0400

    MainActivity done, working on Login

--- app/src/main/java/com/example/roomateapp/LoginActivity.kt
+++ app/src/main/java/com/example/roomateapp/LoginActivity.kt
@@ -1,8 +1,84 @@
 package com.example.roomateapp
 
-class LoginActivity {
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class LoginActivity: Activity() {
+
+    private lateinit var roomText: EditText
+    private lateinit var nameText: EditText
+    private lateinit var loginButton: Button
+    private lateinit var createRoomButton: Button
+
+    private lateinit var sharedPreferences: SharedPreferences
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        sharedPreferences = getSharedPreferences(preferenceKey, Context.MODE_PRIVATE)
+
+        if(/* There is a shared preference */) {
+            setContentView(/* Login Loading Layout */)
+            //TODO: Implement pulling roomcode and user from SharedPreference
+        } else {
+            setContentView(R.layout.login_activity)
+
+            roomText = findViewById<EditText>(R.id.roomcode)
+            nameText = findViewById<EditText>(R.id.name)
+            loginButton = findViewById<Button>(R.id.login)
+            createRoomButton = findViewById<Button>(R.id.create_room)
+
+            loginButton.setOnClickListener{onLoginClick()}
+            createRoomButton.setOnClickListener{onCreateRoomButtonClick()}
+        }
+    }
+
+    private fun onLoginClick() {
+        var roomcode: String = roomText.text.toString().trim()
+        var name: String = nameText.text.toString().trim()
+
+        if(roomcode.isEmpty() || name.isEmpty()) {
+            Toast.makeText(this@LoginActivity, R.string.missing_login_field, Toast.LENGTH_SHORT)
+            return
+        }
+
+        var alertBuilder: AlertDialog.Builder = AlertDialog.Builder(this)
+        alertBuilder.setTitle("Are you sure you want to login with this information?")
+        alertBuilder.setMessage(
+            "Your roomcode is set as $roomcode.\n" +
+            "Your username is set as $name.\n" +
+            "This will permantly assign you this room and username. You cannot change these parameters." +
+            "Are you sure you want to proceed?")
+        alertBuilder.setNegativeButton("Cancel", DialogInterface.OnClickListener {
+          dialog, id -> dialog.cancel()
+        })
+        alertBuilder.setPositiveButton("Login", DialogInterface.OnClickListener {
+            dialog, id -> requestRoom(roomcode, name)
+        })
+
+        val alert = alertBuilder.create()
+        alert.show()
+
+    }
+
+    private fun requestRoom(roomString: String, username: String) {
+
+    }
+
+    private fun onCreateRoomButtonClick() {
+
+    }
 
     companion object {
+        private const val preferenceKey = "RoommateAppLoginDetails"
         private const val TAG = "RoommateApp"
     }
 }
--- app/src/main/java/com/example/roomateapp/MainActivity.kt
+++ app/src/main/java/com/example/roomateapp/MainActivity.kt
@@ -1,6 +1,39 @@
 package com.example.roomateapp
 
-class MainActivity {
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.example.roomateapp.grocery.GroceryManagerActivity
+
+class MainActivity: Activity() {
+
+    private lateinit var groceryButton: Button
+    private lateinit var choresButton: Button
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+
+        Log.i(TAG, "Entered Main Activity.")
+
+        groceryButton = findViewById<Button>(R.id.grocery_button)
+        choresButton = findViewById<Button>(R.id.chores_button)
+
+        groceryButton.setOnClickListener {
+            Log.i(TAG, "Selected Grocery List from Main Activity.")
+
+            var intent: Intent = Intent(this, GroceryManagerActivity::class.java)
+            startActivity(intent)
+        }
+
+        choresButton.setOnClickListener {
+            Log.i(TAG, "Selected Grocery List from Main Activity.")
+
+            var intent: Intent = Intent(this, ChoresManagerActivity::class.java)
+            startActivity(intent)
+        }    }
 
     companion object {
         private const val TAG = "RoommateApp"
