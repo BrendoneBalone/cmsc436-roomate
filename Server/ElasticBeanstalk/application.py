f2ead4740f16196d63fe34519ed71f1380b70543
dlee021 <dlee12321@yahoo.com>
2022-04-29 21:29:36 -0400

    Added grocery routes and documentation

--- Server/ElasticBeanstalk/application.py
+++ Server/ElasticBeanstalk/application.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import string
 import random
 
@@ -8,6 +8,24 @@ application = Flask(__name__)
 # Data
 #################################
 rooms = {} # Key: Roomcode, Value: All Data
+
+'''
+{
+    grocery: {
+        name: {
+            completed: Boolean
+        }
+    },
+    chores: {
+        name: {
+            completed: Boolean
+            assignee: String
+            dates: String[] of Weekdays
+        }
+    },
+    roommates: String[]
+}
+'''
 
 #################################
 # Private Functions
@@ -22,32 +40,143 @@ def generate_roomcode():
     while roomcode in rooms.keys():
         roomcode = generate_random_string()
 
-    rooms["roomcode"] = {"grocery": {}, "chores": {}}
+    rooms[roomcode] = {"grocery": {}, "chores": {}}
 
     return roomcode
 
 #################################
-# Routes
+# "Private" Routes
 #################################
 
 @application.route('/save', methods=["GET"])
 def save():
     return rooms
 
-@application.route('room', methods=["GET"])
+@application.route('/load', methods=["POST"])
+def load():
+    newData = request.json
+    rooms = newData
+    return "ok", 200
+
+
+#################################
+# Routes
+#################################
+
+@application.route('/login', methods=["POST"])
 def room():
-    data = Flask.request.data
+    data = request.json
     if "roomcode" in data:
-        if data["roomcode"] == "new":
-            return generate_roomcode()
-        elif len(data["roomcode"]) == 6:
-            return data["roomcode"]
+        roomcode = data["roomcode"]
+        if roomcode in rooms:
+            if "username" in data:
+                username = data["username"]
+                
+                if username not in rooms[roomcode]["roommates"]:
+                    rooms[roomcode]["roommates"].append(username)
+
+                return {"roomcode": roomcode}, 200
+
+    return "err", 400
+
+@application.route('/new_room_code', methods=["GET"])
+def new_room_code():
+    return {"roomcode": generate_roomcode()}, 200
+
+@application.route('/grocery', methods=["GET"])
+def grocery():
+    data = request.json
+    if "roomcode" in data:
+        roomcode = data["roomcode"]
+        if roomcode in rooms:
+            return rooms[roomcode]["grocery"], 200
     
     return "err", 400
 
-@application.route('grocery', methods=["GET", "POST"])
-def grocery():
-    pass
+@application.route('/grocery/add', methods=["POST"])
+def add_grocery():
+    data = request.json
+    if "roomcode" in data:
+        roomcode = data["roomcode"]
+        if roomcode in rooms:
+            if "groceryName" in data:
+                groceryName = data["groceryName"]
+
+                if groceryName not in rooms[roomcode]["grocery"]:
+                    rooms[roomcode]["grocery"][groceryName]["completed"] = False
+                
+                return rooms[roomcode]["grocery"], 200
+
+    return "err", 400
+
+@application.route('/grocery/complete', methods=["POST"])
+def add_grocery():
+    data = request.json
+    if "roomcode" in data:
+        roomcode = data["roomcode"]
+        if roomcode in rooms:
+            if "groceryName" in data:
+                groceryName = data["groceryName"]
+
+                if groceryName in rooms[roomcode]["grocery"]:
+                    rooms[roomcode]["grocery"][groceryName]["completed"] = True
+                    return rooms[roomcode]["grocery"], 200
+
+    return "err", 400
+
+@application.route('/grocery/uncomplete', methods=["POST"])
+def add_grocery():
+    data = request.json
+    if "roomcode" in data:
+        roomcode = data["roomcode"]
+        if roomcode in rooms:
+            if "groceryName" in data:
+                groceryName = data["groceryName"]
+
+                if groceryName in rooms[roomcode]["grocery"]:
+                    rooms[roomcode]["grocery"][groceryName]["completed"] = False
+                    return rooms[roomcode]["grocery"], 200
+
+    return "err", 400
+    
+@application.route('/roommates', methods=["GET"])
+def roommates():
+    data = request.json
+    if "roomcode" in data:
+        roomcode = data["roomcode"]
+        if roomcode in rooms:
+            return {"roommates": rooms[roomcode]["roommates"]}, 200
+    
+    return "err", 400
+
+@application.route('/chores', methods=["GET"])
+def chores():
+    data = request.json
+    if "roomcode" in data:
+        roomcode = data["roomcode"]
+        if roomcode in rooms:
+            return rooms[roomcode]["chores"], 200
+    
+    return "err", 400
+
+@application.route('/chores/add', methods=["POST"])
+def add_grocery():
+    data = request.json
+    if "roomcode" in data:
+        roomcode = data["roomcode"]
+        if roomcode in rooms:
+            if "choreName" in data:
+                choreName = data["choreName"]
+
+                if choreName not in rooms[roomcode]["chores"]:
+                    rooms[roomcode]["chores"][choreName]["completed"] = False
+                    rooms[roomcode]["chores"][choreName]["assignee"] = None
+                    rooms[roomcode]["chores"][choreName]["date"] = None
+                
+                return rooms[roomcode]["chores"], 200
+
+    return "err", 400
+
 
 
 if __name__ == "__main__":
--- Server/ServerRequests.md
+++ Server/ServerRequests.md
@@ -4,13 +4,103 @@
 The link/url will be created later. All requests should be sent to URL+some route.
 Ex. `someurl.com/room'
 
+We should have a private const variable with the URL.
+
+## Kotlin Syntax (Not yet tested!!)
+To send a post req:
+```
+try {
+
+     val httpclient:HttpClient=DefaultHttpClient()
+     val httppost: HttpPost =HttpPost(URL)
+
+     // nameValuePairs can be an ArrayList<>
+     // This is how you send data to the server
+     httppost.entity = UrlEncodedFormEntity(nameValuePairs)
+
+     val httpresponse:HttpResponse=httpclient.execute(httppost)
+     val httpentity: HttpEntity =httpresponse.entity
+
+     // To check response code
+     httpentity.code
+
+ }
+```
+
+The input data should be sent as a JSON in a GET or POST request. The output of the request should also be a JSON.
+
 ## Routes
 These are the routes implemented and their syntax.
 
-### /room
+### /login
+- POST Request
+    - Purpose: Verifying login information. Adding username to data if not present.
+    - Input JSON: {"roomcode": roomcode, "username": username} 
+        - ex: {"roomcode": H256IQ, "username": "Daniel"}
+    - Output: {"roomcode": roomcode}, Response code 200
+    - Error Output: Response code 400
+        - Nonexistent roomcode
+
+### /new_room_code
 - GET Request
-    - Input Data: {"roomcode": int} 
+    - Purpose: Generates a new, valid roomcode.
+    - Input: None
+    - Output: {"roomcode": roomcode}, Response code 200
+    - Error: Shouldn't happen
+
+### /grocery
+- GET Request
+    - Purpose: Returns a dict with all grocery information
+    - Input JSON: {"roomcode": roomcode}
         - ex: {"roomcode": H256IQ}
-        - If you want a new roomcode send {"roomcode": "new"}
-    - Output: {roomcode}, Response code 200
-        - On error, Response code 400
+    - Output: {groceryName: {"completed": completedBool}}, Response code 200
+        - ex: {"eggs": {"completed": "false"}, "milk": {"completed": "true"}}
+        - I'm pretty sure bools are "true" and "false" in JSON
+    - Error: Response code 400
+        - Nonexistent roomcode
+
+### /grocery/add
+- POST Request
+    - Purpose: Adds a grocery item to data. Sets it's complete value to false.
+    - Input JSON: {"roomcode": roomcode, "groceryName", groceryName}
+        - ex: {"roomcode": H256IQ, "groceryName": "eggs"}
+    - Output: {groceryName: {"completed": completedBool}}, Response code 200
+        - ex: {"eggs": {"completed": "false"}, "milk": {"completed": "true"}}
+        - Updated grocery list
+    - Error: Response code 400
+        - Nonexistent roomcode
+    - Edgecase?:
+        - If an existing grocery item is sent, nothing happens and the response code will be 200
+
+### /grocery/complete
+- POST Request
+    - Purpose: Sets a grocery item's complete value to true.
+    - Input JSON: {"roomcode": roomcode, "groceryName", groceryName}
+        - ex: {"roomcode": H256IQ, "groceryName": "eggs"}
+    - Output: {groceryName: {"completed": completedBool}}, Response code 200
+        - ex: {"eggs": {"completed": "false"}, "milk": {"completed": "true"}}
+        - Updated grocery list
+    - Error: Response code 400
+        - Nonexistent roomcode
+        - Nonexistent grocery item
+
+### /grocery/uncomplete
+- POST Request
+    - Purpose: Sets a grocery item's complete value to false.
+    - Input JSON: {"roomcode": roomcode, "groceryName", groceryName}
+        - ex: {"roomcode": H256IQ, "groceryName": "eggs"}
+    - Output: {groceryName: {"completed": completedBool}}, Response code 200
+        - ex: {"eggs": {"completed": "false"}, "milk": {"completed": "true"}}
+        - Updated grocery list
+    - Error: Response code 400
+        - Nonexistent roomcode
+        - Nonexistent grocery item
+
+### /roommates
+- GET Request
+    - Input JSON: {"roomcode": roomcode}
+        - ex: {"roomcode": H256IQ}
+    - Output: {"roommates": [roommateName]}, Response code 200
+        - ex: {"roommates": ["Daniel", "Logan", "Brendan"]}
+    - Error: Response code 400
+        - Nonexistent roomcode
