4cd192be558b487d9ff87a5f3371d1b289b86e07
Brendan Gillespy <brendan.gillespy@gmail.com>
2022-05-07 02:51:36 -0400

    Switched to using response values to check for sucess

--- app/src/main/java/com/example/roomateapp/grocery/GroceryViewModel.kt
+++ app/src/main/java/com/example/roomateapp/grocery/GroceryViewModel.kt
@@ -9,6 +9,7 @@ import com.google.gson.GsonBuilder
 import com.google.gson.JsonParser
 import io.ktor.client.*
 import io.ktor.client.request.*
+import io.ktor.client.response.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
@@ -45,69 +46,74 @@ class GroceryViewModel: ViewModel() {
     // Returns Map version of grocery list
     fun getGroceryList(roomcode: String): Map<String, *>? {
 
-        var response: Map<String, *>? = null
+        var obj: Map<String, *>? = null
 
         // Launch the request in the background
         job = viewModelScope.launch {
             try {
-                val rawJSON: String = getRequest("grocery/roomcode/$roomcode")
-                response = JSONObject(rawJSON).toMap()
-                _liveData.postValue(response!!)
+                val response = getRequest("grocery/roomcode/$roomcode")
+
+                if(response.status.value in 200..209) {
+                    val rawJSON: String = response.content.toString()
+                    obj = JSONObject(rawJSON).toMap()
+                }
+
+                _liveData.postValue(obj!!)
 
             } catch (e: Exception) {
                 Log.i(TAG, "Error in getGroceryList in GroceryViewModel: ${e.message}")
             }
         }
 
-        return response
+        return obj
     }
 
     // Adds given grocery item to database
     fun addGroceryItem(roomcode: String, item: String): Boolean {
-        var sucess: Boolean = false
+        var success: Boolean = false
 
         job = viewModelScope.launch {
             try {
-                postRequest("grocery/roomcode/$roomcode/name/$item")
-                sucess = true
+                val response = postRequest("grocery/roomcode/$roomcode/name/$item")
+                if (response.status.value in 200..209) success = true
             } catch (e: Exception) {
                 Log.i(TAG, "Error in addGroceryItem in GroceryViewModel: ${e.message}")
             }
         }
 
-        return sucess
+        return success
     }
 
     // Deletes given grocery item from database
     fun deleteGroceryItem(roomcode: String, item: String): Boolean {
-        var sucess: Boolean = false
+        var success: Boolean = false
 
         job = viewModelScope.launch {
             try {
-                deleteRequest("grocery/roomcode/$roomcode/name/$item")
-                sucess = true
+                val response = deleteRequest("grocery/roomcode/$roomcode/name/$item")
+                if(response.status.value in 200..209) success = true
             } catch (e: Exception) {
                 Log.i(TAG, "Error in deleteGroceryItem in GroceryViewModel: ${e.message}")
             }
         }
 
-        return sucess
+        return success
     }
 
     // Creates generic getRequest
-    private suspend fun getRequest(url: String): String =
+    private suspend fun getRequest(url: String): HttpResponse =
         withContext(Dispatchers.IO) {
             HttpClient().get("$SERVER/$url")
         }
 
     // Creates generic postRequest
-    private suspend fun postRequest(url: String): String =
+    private suspend fun postRequest(url: String): HttpResponse =
         withContext(Dispatchers.IO) {
             HttpClient().post("$SERVER/$url")
         }
 
     // Creates generic deleteRequest
-    private suspend fun deleteRequest(url: String): String =
+    private suspend fun deleteRequest(url: String): HttpResponse =
         withContext(Dispatchers.IO) {
             HttpClient().delete("$SERVER/$url")
         }
