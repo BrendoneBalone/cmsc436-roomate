df2687eb79bb4481cd1356273479e41a3a518646
dlee021 <dlee12321@yahoo.com>
2022-05-08 21:21:50 -0400

    added sunday cleanup feature to server

--- Server/sunday_cleanup.py
+++ Server/sunday_cleanup.py
@@ -4,12 +4,4 @@ import json
 # URL = 'http://436env5.eba-vukmr2km.us-east-2.elasticbeanstalk.com'
 # URL = 'http://127.0.0.1:5000'
 
-f = open("data.json")
-jsonData = json.load(f)
-f.close()
-
-data = requests.post(URL+'/load', json=jsonData)
-
-
-
-
+data = requests.post(URL+'/clear_groceries')
