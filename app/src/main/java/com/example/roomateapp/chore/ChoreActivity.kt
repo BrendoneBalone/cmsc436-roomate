d5116e1daedb6627c258f3ca91d90f73154716fd
Logan Krashnak <lak801@yahoo.com>
2022-05-08 21:28:31 -0400

    chore server functionality almost

--- app/src/main/java/com/example/roomateapp/chore/ChoreActivity.kt
+++ app/src/main/java/com/example/roomateapp/chore/ChoreActivity.kt
@@ -27,10 +27,7 @@ class ChoreActivity : FragmentActivity() {
     private lateinit var mDate: Date
     private lateinit var mStatusRadioGroup: RadioGroup
     private lateinit var mTitleText: EditText
-    private lateinit var mDefaultStatusButton: RadioButton
-    private lateinit var mDefaultPriorityButton: RadioButton
     private lateinit var dateView: TextView
-    private lateinit var timeView: TextView
 
     private var roomcode: String? = null
     private var username: String? = null
@@ -38,28 +35,19 @@ class ChoreActivity : FragmentActivity() {
     private val status: Status
         get() {
 
-            return when (mStatusRadioGroup.checkedRadioButtonId) {
-                 R.id.statusDone-> {
-                    Status.DONE
-                }
-                else -> {
-                    Status.NOTDONE
-                }
-            }
+          return  Status.NOTDONE
         }
 
+
+    private val weekday = arrayOf(" ","SUN","MON","TUE","WED","THU","FRI","SAT")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_chore)
-
         roomcode = intent.getStringExtra("roomcode")
         username = intent.getStringExtra("username")
 
         mTitleText = findViewById<View>(R.id.chore_title) as EditText
-        mDefaultStatusButton = findViewById<View>(R.id.statusNotDone) as RadioButton
-        mStatusRadioGroup = findViewById<View>(R.id.statusGroup) as RadioGroup
         dateView = findViewById<View>(R.id.date) as TextView
-        timeView = findViewById<View>(R.id.time) as TextView
 
         // Set the default date and time
 
@@ -76,8 +64,6 @@ class ChoreActivity : FragmentActivity() {
         // show
         // the Time Dialog
 
-        val timePickerButton = findViewById<View>(R.id.time_picker_button) as Button
-        timePickerButton.setOnClickListener { showTimePickerDialog() }
 
         // OnClickListener for the Cancel Button,
 
@@ -98,7 +84,6 @@ class ChoreActivity : FragmentActivity() {
             //Reset data to default values
             setDefaultDateTime()
             mTitleText.text = null
-            mStatusRadioGroup.check(mDefaultStatusButton.id)
         }
 
         // Set up OnClickListener for the Submit Button
@@ -108,22 +93,18 @@ class ChoreActivity : FragmentActivity() {
             Log.i(TAG, "Entered submitButton.OnClickListener.onClick()")
 
             // Get Status
-            val statusData = status.toString()
 
             // Title
             val titleData = mTitleText.text.toString()
             // Date
 
-            val dateData = dateView.text
+            val dateData = dateView.text.toString()
 
-
-            val timeData = timeView.text.toString()
             // Package ToDoItem data into an Intent
             val dataIntent: Intent = Intent().apply {
                 putExtra("title",titleData)
-                putExtra("status",statusData)
+                putExtra("status",Status.NOTDONE)
                 putExtra("date",dateData)
-                putExtra("time",timeData)
                 putExtra("roomcode", roomcode)
                 putExtra("username", username)
             }
@@ -142,21 +123,17 @@ class ChoreActivity : FragmentActivity() {
 
         val c = Calendar.getInstance()
         c.time = mDate
-
         setDateString(c.get(Calendar.YEAR), c.get(Calendar.MONTH),
                 c.get(Calendar.DAY_OF_MONTH))
 
-        dateView.text = dateString
+        dateView.text = weekday[c.get(Calendar.DAY_OF_WEEK)]
 
-        setTimeString(c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE))
-
-        timeView.text = timeString
     }
 
     // DialogFragment used to pick a ToDoItem deadline date
 
     class DatePickerFragment : DialogFragment(), DatePickerDialog.OnDateSetListener {
-
+        private val weekday = arrayOf(" ","SUN","MON","TUE","WED","THU","FRI","SAT")
         override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
 
             // Use the current date as the default date in the picker
@@ -173,47 +150,26 @@ class ChoreActivity : FragmentActivity() {
                                dayOfMonth: Int) {
             setDateString(year, monthOfYear, dayOfMonth)
             val dateView: TextView = requireActivity().findViewById(R.id.date)
-            dateView.text = dateString
+            val c = Calendar.getInstance()
+            c.set(year,monthOfYear,dayOfMonth)
+            dateView.text = weekday[c.get(Calendar.DAY_OF_WEEK)]
         }
     }
 
     // DialogFragment used to pick a ToDoItem deadline time
-
-    class TimePickerFragment : DialogFragment(), TimePickerDialog.OnTimeSetListener {
-
-        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-
-            // Use the current time as the default values for the picker
-            val c = Calendar.getInstance()
-            val hour = c.get(Calendar.HOUR_OF_DAY)
-            val minute = c.get(Calendar.MINUTE)
-
-            // Create a new instance of TimePickerDialog and return
-            return TimePickerDialog(activity, this, hour, minute, true)
-        }
-
-        override fun onTimeSet(view: TimePicker, hourOfDay: Int, minute: Int) {
-            setTimeString(hourOfDay, minute)
-            val timeView: TextView = requireActivity().findViewById(R.id.time)
-            timeView.text = timeString
-        }
-    }
 
     private fun showDatePickerDialog() {
         val newFragment = DatePickerFragment()
         newFragment.show(supportFragmentManager, "datePicker")
     }
 
-    private fun showTimePickerDialog() {
-        val newFragment = TimePickerFragment()
-        newFragment.show(supportFragmentManager, "timePicker")
-    }
+
 
     companion object {
 
         // 7 days in milliseconds - 7 * 24 * 60 * 60 * 1000
         private const val SEVEN_DAYS = 604800000
-
+        private val weekday = arrayOf(" ","SUN","MON","TUE","WED","THU","FRI","SAT")
         private const val TAG = "RoommateApp"
 
         private var timeString: String? = null
